f0027229b2e3cf78aea10d3590617a91ddff6394
nizar <kurniawan.nizar.ploso@gmail.com>
2021-05-24 07:15:13 +0700

    Ngrembel APP

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                Intent i = new Intent(MainActivity.this,MenuActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
