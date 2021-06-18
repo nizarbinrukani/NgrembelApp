bf0533717578a013ae480ea16c570d6465fb8d5c
Denny Cahyo W <dennycw19@gmail.com>
2021-06-18 13:57:33 +0700

    Tambah Halaman Show Account

---
+++ app/src/main/java/com/example/myapplication/ui/ShowAccount.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+
+public class ShowAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_account);
+    }
+}
